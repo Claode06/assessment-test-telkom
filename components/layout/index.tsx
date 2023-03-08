4aa67c366bcd1e9aa936375b9980a4c52411e4b3
Claode06 <stevenclaode64@gmail.com>
2023-03-09 01:01:15 +0700

    table user github into home page

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -21,11 +21,47 @@ const Layout: React.FC<props> = ({ children }) => {
         <div className=" font-semibold text-white">
           {/* <Link href={"/repo"}>List Repository</Link> */}
           <button className="hover:text-slate-800 text-slate-200 transition duration-200">
-            <Link href={"/about"}>About Me</Link>
+            <a href={"https://docs.github.com/"} target="_blank">
+              Documentation
+            </a>
           </button>
         </div>
       </div>
-      <div className="h-full pb-20 overflow-auto">{children}</div>
+      <div className=" h-full overflow-auto">
+        <div>{children}</div>
+        <div className=" pb-20">
+          <div className="bg-slate-700 px-32  py-4  flex justify-between items-center">
+            <p className="font-semibold text-slate-300">
+              &copy; Copyright - Steven Claode
+            </p>
+            <div className="flex">
+              <a
+                href="https://www.instagram.com/steven_claode/"
+                target="_blank"
+              >
+                <Image
+                  src={"/img/ig.png"}
+                  alt="instagram"
+                  height={20}
+                  width={30}
+                />
+              </a>
+              <a
+                href="https://github.com/claode06"
+                className="ml-4"
+                target="_blank"
+              >
+                <Image
+                  src={"/img/git.png"}
+                  alt="instagram"
+                  height={20}
+                  width={30}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import DescriptionComponent from "@/components/home/description";
+import Repositories from "@/components/home/repositories";
 import { useStateContext } from "@/context";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,38 +14,9 @@ const HomePage: React.FC = () => {
     });
   }, []);
   return (
-    <div className="px-32 bg-white1">
-      <div className="py-16 flex items-center">
-        <Image
-          alt="github"
-          src={"/img/github_image.png"}
-          height={300}
-          width={300}
-        />
-        <div className="ml-4">
-          <p className="text-justify indent-8">
-            <a
-              className="font-bold"
-              href={"https://github.com/"}
-              target="_blank"
-            >
-              GitHub
-            </a>{" "}
-            adalah platform pengembangan perangkat lunak berbasis web yang
-            memungkinkan pengguna untuk menyimpan, mengelola, dan berbagi kode
-            sumber secara online. GitHub digunakan oleh para pengembang
-            perangkat lunak untuk berkolaborasi pada proyek-proyek open-source
-            maupun proyek-proyek swasta. Platform ini juga menawarkan
-            fitur-fitur seperti pelacakan perubahan kode, manajemen tugas, dan
-            integrasi dengan berbagai alat pengembangan perangkat lunak. Selain
-            itu, GitHub juga menawarkan fitur sosial seperti forum diskusi,
-            profil pengguna, dan fitur pencarian kode sumber. GitHub merupakan
-            salah satu platform yang paling populer untuk pengembangan perangkat
-            lunak dan digunakan oleh banyak organisasi besar dan kecil di
-            seluruh dunia.
-          </p>
-        </div>
-      </div>
+    <div className="">
+      <DescriptionComponent />
+      <Repositories />
     </div>
   );
 };
